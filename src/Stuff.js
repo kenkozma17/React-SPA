847cc398524e11b49c7879ed703733d725f87274
kenkozma17 <kenkozma17@gmail.com>
2019-03-03 15:31:30 +0800

    Uploading project

---
+++ src/Stuff.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+ 
+class Stuff extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Stuff Page</h2>
+      </div>
+    );
+  }
+}
+ 
+export default Stuff;
